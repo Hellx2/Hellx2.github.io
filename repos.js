6927f14781415a6251ce10d3cc91fdf04c94df9d
Your Name <you@example.com>
2024-04-25 15:03:45 +1000

    Fix links.

--- repos.js
+++ repos.js
@@ -4,6 +4,6 @@ for (child of list.children) {
     let text = child.innerText;
     let parts = text.split(":", 3);
     // NOTE: Remove `target="_blank"` once testing done
-    child.onclick = () => window.open(`https://github.com/Hellx2/${parts[0]}`, "_blank");
-    child.innerHTML = `<a href="https://github.com/Hellx2/${parts[0]}" target="_blank">${parts[1]}</a><br>${parts[2]}`
+    child.onclick = () => window.open(`https://github.com/Hellx2/${parts[0]}`, "_self");
+    child.innerHTML = `<a href="https://github.com/Hellx2/${parts[0]}" target="_self">${parts[1]}</a><br>${parts[2]}`
 }
